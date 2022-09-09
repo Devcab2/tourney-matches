8bdab209a2b6325fe63ade89dff545a4ec305924
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-09-09 16:15:24 -0600

    setup tourney app and create basic components for data structure

---
+++ src/components/MatchList.js
@@ -0,0 +1,6 @@
+import React from "react"; //optional
+
+<section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      {/* Matches will be shown here */}
+</section>
---
+++ src/components/PlayerList.js
@@ -0,0 +1,6 @@
+import React from "react"; //optional
+
+<section classsName="PlayerList">
+  <h1>Current participating players</h1>
+  {}
+</section>
